ae5ff5def04d55a07b1eb3cec9b124fb17b8497b
pablinho <p.olmedosaez@gmail.com>
2022-02-09 15:28:52 -0400

    Correjimos algunos errores e intalamos un paquete para que nuesta pagina sea visible en github pages

--- src/components/contacto.js
+++ src/components/contacto.js
@@ -5,7 +5,6 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import ContactoContenido from "./contactoContenido";
-
 export default function BotonContacto() {
   const [open, setOpen] = React.useState(false);
 
